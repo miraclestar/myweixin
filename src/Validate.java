9b095db42642ed57a5bcc61a005ae05a9230864c
miraclestar <liuhongyuan99@qq.com>
2013-04-18 15:16:52 +0800

    ad.html

--- src/Validate.java
+++ src/Validate.java
@@ -20,7 +20,6 @@ public class Validate extends HttpServlet {
 
 	public Validate() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
