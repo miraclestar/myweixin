865112640e11bc9434beb6736ee49849d8df470e
miraclestar <liuhongyuan99@qq.com>
2013-07-03 19:25:55 +0800

    baidu

--- src/info/xmark/base/DBPool.java
+++ src/info/xmark/base/DBPool.java
@@ -1,13 +1,10 @@
 package info.xmark.base;
 
-import java.beans.PropertyVetoException;
-import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Properties;
 
 import org.apache.log4j.Logger;
 
@@ -27,14 +24,19 @@ public class DBPool {
 	public DBPool() {
 		try {
 			log.debug("1:initial");
-			InputStream is = DBPool.class.getResourceAsStream("/jdbc.properties");
-			Properties p = new Properties();
-			p.load(is);
-			log.debug("2:loadproperties " + is.toString());
-			dbUrl = p.getProperty("cdn.url");
-			dbUser = p.getProperty("cdn.username");
-			dbPwd = p.getProperty("cdn.password");
-			String driver = p.getProperty("driver");
+			// （2）直接从请求header中获取ip、端口、用户名和密码信息
+			// String host = request.getHeader("BAE_ENV_ADDR_SQL_IP");
+			// String port = request.getHeader("BAE_ENV_ADDR_SQL_PORT");
+			// String username = request.getHeader("BAE_ENV_AK");
+			// String password = request.getHeader("BAE_ENV_SK");
+			// （3）从线程变量BaeEnv接口获取ip、端口、用户名和密码信息
+			// 从平台查询应用要使用的数据库名
+
+			dbUrl = "jdbc:mysql://" + System.getenv("BAE_ENV_ADDR_SQL_IP") + ":" + System.getenv("BAE_ENV_ADDR_SQL_PORT") + "/" + "wTnuNwxewwFbuooQmTmu";
+			dbUser = System.getenv("BAE_ENV_AK");
+			dbPwd = System.getenv("BAE_ENV_SK");
+
+			String driver = "com.mysql.jdbc.Driver";
 			dataSource = new ComboPooledDataSource();
 			log.debug("3:dataSource " + dbUrl + "|" + dbUser + "|" + dbPwd);
 			dataSource.setUser(dbUser);
@@ -46,16 +48,31 @@ public class DBPool {
 			dataSource.setMaxPoolSize(20);
 			dataSource.setMaxIdleTime(15);
 			dataSource.setAcquireIncrement(5);
-			is.close();
-		} catch (PropertyVetoException e) {
-			e.printStackTrace();
-			log.error(e.getMessage());
+
 		} catch (Exception e) {
 			e.printStackTrace();
 			log.error(e.getMessage());
 		}
 	}
 
+	/*
+	 * 
+	 * public DBPool() { try { log.debug("1:initial"); InputStream is =
+	 * DBPool.class.getResourceAsStream("/jdbc.properties"); Properties p = new
+	 * Properties(); p.load(is); log.debug("2:loadproperties " + is.toString());
+	 * dbUrl = p.getProperty("cdn.url"); dbUser = p.getProperty("cdn.username");
+	 * dbPwd = p.getProperty("cdn.password"); String driver =
+	 * p.getProperty("driver"); dataSource = new ComboPooledDataSource();
+	 * log.debug("3:dataSource " + dbUrl + "|" + dbUser + "|" + dbPwd);
+	 * dataSource.setUser(dbUser); dataSource.setPassword(dbPwd);
+	 * dataSource.setJdbcUrl(dbUrl); dataSource.setDriverClass(driver);
+	 * dataSource.setInitialPoolSize(5); dataSource.setMinPoolSize(5);
+	 * dataSource.setMaxPoolSize(20); dataSource.setMaxIdleTime(15);
+	 * dataSource.setAcquireIncrement(5); is.close(); } catch
+	 * (PropertyVetoException e) { e.printStackTrace();
+	 * log.error(e.getMessage()); } catch (Exception e) { e.printStackTrace();
+	 * log.error(e.getMessage()); } }
+	 */
 	public final static DBPool getInstance() {
 		log.debug("4:get instance");
 		return dbPool;
